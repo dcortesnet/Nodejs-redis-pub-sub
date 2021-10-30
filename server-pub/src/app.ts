8997aa3f9cd2e2be8899d6dfac23bca9eca1ddc9
Diego Cortés <dcortes@agilesoft.cl>
2021-10-30 13:51:54 -0300

    :sparkles: feat: Docker compose server1 server2

--- server-pub/src/app.ts
+++ server-pub/src/app.ts
@@ -1,6 +1,20 @@
 import express from 'express';
+import redis from 'redis';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+
+const clientRedis = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD
+});
+
+clientRedis.on('error', err => {
+  console.log('Redis error', err)
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -8,6 +22,7 @@ app.use(express.json());
 app.get('/', (req, res) => {
   return res.send('Hello world');
 });
+
 
 app.listen(3001, () => {
   console.log('Server running');
